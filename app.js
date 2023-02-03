48e97302934d6817a99bf2744af923213352a868
Said Cemal Türkdoğan <69085609+saidturkdogan@users.noreply.github.com>
2023-02-04 02:04:29 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+
+let x = prompt("Please enter a number:")
+
+convertDecimalToBinary(x)
+
+function convertDecimalToBinary(number) {
+    let binary =""
+    while (true) {
+        binary+=(number%2).toString()
+        number=Math.floor(number/2)
+        if (number==1) {
+            binary+=1
+            break
+        }
+    }
+
+    let result = reverse(binary)
+    console.log("Result : "+ result)
+}
+
+
+
+
+function reverse(binary) {
+    let reverseBinary = ""
+    for(let i = binary.length-1 ; i>=0 ; i--){
+        reverseBinary+=binary.charAt(i)
+    }
+    
+    return reverseBinary
+    
+}
